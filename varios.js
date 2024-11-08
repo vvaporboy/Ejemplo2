93ed6b85e00115d58db717d2288d01520373e74e
Alex Aguirre <vaporboysaveus@gmail.com>
2024-11-08 18:31:38 -0300

    act
    act 8.11.24

--- varios.js
+++ varios.js
@@ -1,28 +1,32 @@
 
 // funcion para escuchar un evento
 function iniciarFormulario() {
-    const form = document.getElementById('empezarencargue'); // toma el formulario por id
+    const form = document.getElementById('empezarencargue');
     if (form) {
-        form.addEventListener('submit', enviarDatos); // asocia el evento 'submit' al formulario
+        form.addEventListener('submit', enviarDatos);
     }
 }
 //funcion para enviar datos
 
 function enviarDatos(event) {
     event.preventDefault(); // Evita el envío del formulario
+    window.location.href = 'encargoenviado.html';
 
     // valores del formulario
-    const email = document.getElementById('email').value; //importante naturalizar sintaxis, se usa asi siempre para tomar elementos
+    const archivo = document.getElementById('archivo').value;
     const size = document.getElementById('size').value;
     const papel = document.getElementById('papel').value;
-    const archivo = document.getElementById('archivo').value;
-
+    const cantidad = document.getElementById('cantidad').value;
+    const email = document.getElementById('email').value;
+    const telefono = document.getElementById('telefono').value;
+    const domicilio = document.getElementById('domicilio').value;
+    const codigopostal = document.getElementById('codigopostal').value;
     // objeto con los datos
-    const data = { email, password };
-    console.log(data);  // Mostrar los datos en la consola
+    const data = { archivo, size, papel, cantidad, email, telefono, domicilio, codigopostal };
+    console.log(data);
 
     // respuesta exitosa
     console.log('Datos enviados correctamente'); // imprime en consola, se ve apretando f12 en el navegador y yendo a consola
     alert('Datos enviados correctamente para el email: ' + email); // Muestra msj en navegador 
-    window.location.href = 'encargoenviado.html';
+
     }
