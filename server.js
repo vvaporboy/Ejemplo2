5926af246903fd36a14eea3047baee9225ac1c84
Alex Aguirre <vaporboysaveus@gmail.com>
2024-11-12 20:05:42 -0300

    V9
    V9

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta de ejemplo
 app.get('/', (req, res) => {
@@ -11,7 +13,7 @@ app.get('/', (req, res) => {
 
 // Ruta para servir el archivo logueo.html
 app.get('/inicio', (req, res) => {
-  res.sendFile(path.join(__dirname,'encargarimpresion.html'));  // Asegúrate de que 'logueo.html' esté dentro de 'public'
+  res.sendFile(path.join(__dirname, 'public','encargarimpresion.html'));  // Asegúrate de que 'logueo.html' esté dentro de 'public'
 });
 
 // Iniciar el servidor
